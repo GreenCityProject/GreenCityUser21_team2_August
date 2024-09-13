63d82fa254518604057942e689de0c0b92931312
Andrii Simanovskyi <simanoav@gmail.com>
2024-09-13 16:00:00 +0300

    [13] added oauth-client dependency to service-api

--- service-api/src/main/java/greencity/service/CustomOauth2UserService.java
+++ service-api/src/main/java/greencity/service/CustomOauth2UserService.java
@@ -1,7 +1,6 @@
 package greencity.service;
 
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 public interface CustomOauth2UserService {
